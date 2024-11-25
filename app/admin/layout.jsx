bed89deff3c5347a835be08bd7242340ecbc9af3
Vani Gupta <vani.gupta@altudo.co>
2024-11-25 11:11:58 +0530

    c2

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,14 +1,14 @@
 import Sidebar from "@/components/AdminComponents/Sidebar"
 import Image from "next/image"
 import { imgs } from "@/Imgs/imgs"
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer} from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout({children}){
     return(
         <>
         <div className="flex">
-            <ToastContainer theme="dark"/>
+            {/* <ToastContainer theme="dark"/> */}
             <Sidebar/>
             <div className="flex flex-col w-full">
                 <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border border-black">
