10cc242b4c43ce7fc0c668780abc6e6119d9c7de
Vani Gupta <vani.gupta@altudo.co>
2024-11-30 17:59:15 +0530

    layout changes

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 
-const Blog_item = ({ title, description, category, image, id }) => {
+const BlogItem = ({ title, description, category, image, id }) => {
   return (
-    <div className=" text-center max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
+    <div className="text-center max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000] min-h-[450px] flex flex-col justify-between">
       <Link href={`/blogs/${id}`}>
         <Image
           src={image}
@@ -15,17 +15,17 @@ const Blog_item = ({ title, description, category, image, id }) => {
           className="border border-black"
         ></Image>
 
-        <p className=" mt-5 px-1 inline-block bg-black text-white text-l">
+        <p className="mt-5 px-1 inline-block bg-black text-white text-l">
           {category}
         </p>
         <div className="p-5">
-          <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-900">
+          <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-900 truncate">
             {title}
           </h5>
-          <p className="mb-3 text-sm tracking-tight text-gray-700">
+          <p className="mb-3 text-sm tracking-tight text-gray-700 line-clamp-3">
             {description}
           </p>
-          <div className="inline-flex items-center py-2 font-semibold text-center">
+          <div className="mt-auto inline-flex items-center py-2 font-semibold text-center">
             Read more{" "}
             <Image src={imgs.arrow} className="ml-2" width={20} alt="" />
           </div>
@@ -34,4 +34,4 @@ const Blog_item = ({ title, description, category, image, id }) => {
     </div>
   );
 };
-export default Blog_item;
+export default BlogItem;
